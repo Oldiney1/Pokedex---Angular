52984d13ed2cc02de20234f7ba1631470927453f
oldiney <oldineygalvao@gmail.com>
2025-05-08 21:31:21 -0300

    fix: search input funcional

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,86 +1,93 @@
-  // home.component.ts
-  import { Component, OnInit } from '@angular/core';
-  import { PokeAPIService } from '../../services/poke-api.service';
-  import { CommonModule } from '@angular/common';
+// home.component.ts
+import { Component, OnInit } from '@angular/core';
+import { PokeAPIService } from '../../services/poke-api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-  @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports: [CommonModule]  
-  })
-  export class HomeComponent implements OnInit {
-    pokemonAtual: any;
-    pokemonAnterior: any;
-    pokemonProximo: any;
-  
-    private apiUrlImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-    private apiUrlIcon = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/';
-    private apiUrlGif = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/';
-  
-    constructor(private pokeApiService: PokeAPIService) {}
-  
-    ngOnInit(): void {
-      this.carregarPokemon(2); // Começa pelo ID 1
-    }
-  
-    carregarPokemon(id: number): void {
-      this.pokeApiService.getPokemonById(id).subscribe(pokemon => {
-        this.pokemonAtual = pokemon;
-  
-        // Carregar anterior
-        if (id > 1) {
-          this.pokeApiService.getPokemonById(id - 1).subscribe(pokeAnt => {
-            this.pokemonAnterior = pokeAnt;
-          });
-        } else {
-          this.pokemonAnterior = null;
-        }
-  
-        // Carregar próximo
-        if (id < 151) {
-          this.pokeApiService.getPokemonById(id + 1).subscribe(pokeProx => {
-            this.pokemonProximo = pokeProx;
-          });
-        } else {
-          this.pokemonProximo = null;
-        }
-      });
-    }
-  
-    getPokemonImageUrl(id: number): string {
-      return `${this.apiUrlImage}/${id}.png`;
-    }
-  
-    getPokemonImageGif(id: number): string {
-      return `${this.apiUrlGif}/${id}.gif`;
-    }
-  
-    getPokemonImageIcon(id: number): string {
-      return `${this.apiUrlIcon}/${id}.png`;
-    }
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
+})
+export class HomeComponent implements OnInit {
+  pokemonAtual: any;
+  pokemonAnterior: any;
+  pokemonProximo: any;
 
-    busca: string = ''; // ngModel do input
+  private apiUrlImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+  private apiUrlIcon = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/';
+  private apiUrlGif = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/';
 
-buscarPokemon(): void {
-  const termo = this.busca.trim().toLowerCase();
-  if (!termo) return;
+  constructor(private pokeApiService: PokeAPIService) { }
 
-  this.pokeApiService.getPokemonByIdOrName(termo).subscribe(pokemon => {
+  ngOnInit(): void {
+    this.carregarPokemon(2);
+  }
+
+  carregarPokemon(id: number): void {
+    this.pokeApiService.getPokemonById(id).subscribe(pokemon => {
+      this.pokemonAtual = pokemon;
+
+      if (id > 1) {
+        this.pokeApiService.getPokemonById(id - 1).subscribe(pokeAnt => {
+          this.pokemonAnterior = pokeAnt;
+        });
+      } else {
+        this.pokemonAnterior = null;
+      }
+
+      if (id < 151) {
+        this.pokeApiService.getPokemonById(id + 1).subscribe(pokeProx => {
+          this.pokemonProximo = pokeProx;
+        });
+      } else {
+        this.pokemonProximo = null;
+      }
+    });
+  }
+
+  getPokemonImageUrl(id: number): string {
+    return `${this.apiUrlImage}/${id}.png`;
+  }
+
+  getPokemonImageGif(id: number): string {
+    return `${this.apiUrlGif}/${id}.gif`;
+  }
+
+  getPokemonImageIcon(id: number): string {
+    return `${this.apiUrlIcon}/${id}.png`;
+  }
+
+  busca: string = '';
+
+  buscarPokemon(): void {
+    const termo = this.busca.trim().toLowerCase();
+    if (!termo) return;
+  
+    this.pokeApiService.getPokemonByIdOrName(termo).subscribe(
+      pokemon => {
+        this.carregarPokemon(pokemon.id);
+        this.busca = '';
+      },
+      err => {
+        console.error('Pokémon não encontrado.');
+        window.alert('Pokémon não encontrado');
+        this.busca = '';
+        this.pokemonAtual = this.pokemonAnterior = this.pokemonProximo = null;
+  
+        this.carregarPokemon(2); // 👈 Volta para o Pokémon 2
+      }
+    );
+  }
+  
+  
+  
+
+  setPokemonAtual(pokemon: any): void {
     this.carregarPokemon(pokemon.id);
-  }, err => {
-    console.error('Pokémon não encontrado.');
-    this.pokemonAtual = this.pokemonAnterior = this.pokemonProximo = null;
-  });
-}
-
-setPokemonAtual(pokemon: any): void {
-  this.carregarPokemon(pokemon.id);
-}
-
-
   }
 
   
-  
+}
